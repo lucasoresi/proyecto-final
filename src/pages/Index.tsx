20517767db857834f76d4360a59f1691bff72919
Giuseppe Giovanelli <giusegio2004@gmail.com>
2025-10-29 20:36:11 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,6 @@ import About from '@/components/About';
 import Testimonials from '@/components/Testimonials';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
-import TurnosAdminPublic from '@/components/TurnosAdminPublic';
 
 const Index = () => {
   return (
